48779cdf6c3e98fc543c7af99b19ef2fbf9584fe
Cody Rickman <crickman@myvessel.com>
2019-07-19 18:14:29 -0400

    First Commit

--- src/main/java/com/creatovemindstudios/trax/Locotmotive/Locomotive.java
+++ src/main/java/com/creatovemindstudios/trax/Locotmotive/Locomotive.java
@@ -1,6 +1,37 @@
 package com.creatovemindstudios.trax.Locotmotive;
 
-public class Locomotive {
+import java.time.LocalTime;
+import java.util.HashMap;
 
+public class Locomotive {
+    private String uuid;
+    public Locomotive(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public HashMap getStatus() {
+
+        return new HashMap();
+    }
+
+    public void throttle (int speed) {
+
+    }
+
+    public void estop () {
+
+    }
+
+    public void stop () {
+
+    }
+
+    public void forward () {
+
+    }
+
+    public void reverse () {
+
+    }
 
 }
--- src/main/java/com/creatovemindstudios/trax/particle/API.java
+++ src/main/java/com/creatovemindstudios/trax/particle/API.java
@@ -1,4 +1,103 @@
 package com.creatovemindstudios.trax.particle;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import elemental.json.Json;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class API {
+
+    static JsonArray getRequest() {
+        try {
+            String url = "http://" + this.mod_properties.getProperty("mod_ip") + ":8080/rest/things/";
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            // optional default is GET
+            con.setConnectTimeout(3000);
+            con.setRequestMethod("GET");
+
+            //add request header
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            con.setRequestProperty("Accept", "application/json");
+            con.setRequestProperty("Authorization", "Bearer " + "");
+
+            int responseCode = con.getResponseCode();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            //print result
+            //System.out.println(response.toString());
+            JsonArray resp = new JsonParser().parse(response.toString()).getAsJsonArray();
+            return resp;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new JsonArray();
+    }
+
+    static JsonObject postRequest() {
+        try {
+            String url = "/";
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            // optional default is GET
+            con.setConnectTimeout(3000);
+            con.setRequestMethod("POST");
+
+            //add request header
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            con.setRequestProperty("Accept", "application/json");
+            con.setRequestProperty("Authorization", "Bearer " + "");
+
+            String urlParameters = "";
+            // Send put request
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            //print result
+            //System.out.println(response.toString());
+            JsonObject resp = new JsonParser().parse(response.toString()).getAsJsonObject();
+            return resp;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new JsonObject();
+    }
 }
